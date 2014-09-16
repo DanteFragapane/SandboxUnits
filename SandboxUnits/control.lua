6f551363799aefa51e25326edd9893b88e52818e
dante123113 <dante.fragapane@gmail.com>
2014-09-16 13:34:29 -0400

    First Commit
    Extract Entire contained SandboxUnits folder into your Factorio Mods
    folder!

---
+++ SandboxUnits/control.lua
@@ -0,0 +1,26 @@
+
+
+
+
+function showItemSearch(key)
+  glob.active_search = key
+	tmgui = game.player.gui.top.tm_flow
+  if tmgui.tmframe.iflow.itemlist_t and tmgui.tmframe.iflow.itemlist_t.valid then
+    tmgui.tmframe.iflow.itemlist_t.destroy()
+  end
+	tmgui.tmframe.iflow.add({ type="table", name="itemlist_t", colspan=ITEM_COLSPAN })
+
+  local items = {}
+	for _,v in pairs(game.itemprototypes) do
+    if game.getlocaliseditemname(v.name):lower():find(key:lower(), 1, true) then
+      table.insert(items, v.name)
+    end
+  end
+  
+  table.sort(items, function(a,b) return game.getlocaliseditemname(a) < game.getlocaliseditemname(b) end)
+	for _,name in ipairs(items) do
+    addItemButton(tmgui.tmframe.iflow.itemlist_t, name)
+	end
+  
+  add_footer(tmgui.tmframe.iflow)
+end
---
+++ SandboxUnits/prototypes/entity.lua
@@ -0,0 +1,11 @@
+data:extend({
+{
+	type = "item",
+	name = "Sandbox-Item",
+	icon = "",
+	flags = {"placeable-neutral","placeable-player","player-creation"},
+	minanble = {hardness = 0.2, mining_time = 0.5, result = ""},
+	max_health = 500,
+	corpse = "Sandbox-Item"
+	collision_box = {{-0.4,-0.4}{0.4,0.4}},
+	selection_box = {{-0.5,-0.5}{0.5,0.5}},
